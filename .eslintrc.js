10af46cc896e4efee4ef6473cfed8bc9a85a26b7
Paulo Avramenko <pavlo.avramenko@moteefe.com>
2020-11-30 00:18:18 +0000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    browser: true,
+    es2020: true,
+    node: true,
+    jest: true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single']
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+const HomePage = () => 'start'
+
+export default HomePage
