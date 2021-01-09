6530b8e8a226a81007235ee99cb7f78ac6186c79
Paulo Avramenko <pavlo.avramenko@moteefe.com>
2021-01-09 19:50:12 +0000

    feat: add resume

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -58,7 +58,26 @@ const Experience = () => {
           </motion.div>
         ))}
       </motion.div>
-      <div className="text-center mt-5 lg:mt-auto font-medium text-2xl">See more</div>
+      <motion.a
+        className="flex justify-center mt-5 lg:mt-auto font-medium text-2xl"
+        initial="hidden"
+        animate="visible"
+        variants={title}
+        href="/paulo-avra-resume-2021.pdf"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Read more on my resume
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+          <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+        </svg>
+      </motion.a>
     </div>
   )
 }
